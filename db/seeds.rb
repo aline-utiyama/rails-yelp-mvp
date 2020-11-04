3ebc6b48785cb1fc82e7ca2063beff6f77c21377
aline utiyama <lineskutiyama@gmail.com>
2020-11-04 13:02:47 +0900

    Half done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating"
+Restaurant.destroy_all
+
+5.times do
+  Restaurant.create!(
+    name: Faker::Coffee.blend_name,
+    address: Faker::Coffee.origin,
+    phone_number: "080-1234-5678",
+    category: Restaurant::CATEGORIES.sample
+    )
+
+end
+puts "created"
