832021eb0f7a8c74fe353e287f3e15e1a20cb319
aline utiyama <lineskutiyama@gmail.com>
2020-11-04 16:20:00 +0900

    Yelp done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  CATEGORIES = ["Chinese", "Italian", "Japanese", "French", "Belgian"]
   validates :name, :address, :category, presence: true
   validates :category, inclusion: {in: CATEGORIES}
 end
